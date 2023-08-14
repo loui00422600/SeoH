ea1c4c8a9eba9787c1ef7940349ae626ec34e469
SeoH <loui000@naver.com>
2023-08-14 18:28:15 +0900

    열번째

---
+++ resources/js/myJs.js
@@ -0,0 +1,11 @@
+// script 태그 내에 작성하던 javaScript 코드 작성
+function f_rgbColor() {
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+
+
+    return "rgb(" + red + "," + green + "," + blue + ")";
+};
+
+const v_phone = "010-1234-5678"
